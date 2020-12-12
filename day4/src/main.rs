16e7e1b057731d7c820a46308bcae4c7b040b354
Michael Joyce <Michael.J.Joyce@jpl.nasa.gov>
2020-12-12 15:56:01 -0800

    day4 cleanup / rework
    Make stuff a bit more "object oriented" by moving passport stuff into a
    struct. Still a bit wasteful given that we don't need to keep any of the
    data around past the first load and verify calls. Seems to be a pattern
    with the problems so far ...

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -4,12 +4,24 @@ use std::io::{BufRead, BufReader, Result};
 use std::ops::RangeInclusive;
 use std::str::FromStr;
 
+use lazy_static::lazy_static;
 use regex::Regex;
 
 // Shameless stolen from SO ...
 macro_rules! vec_of_strings {
     ($($x:expr),*) => (vec![$($x.to_string()),*]);
 }
+
+lazy_static! {
+    static ref REQUIRED_FIELDS: Vec<String> = vec_of_strings!["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+    static ref EYE_COLORS: Vec<String> = vec_of_strings!["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+    static ref PID_RGX: Regex = Regex::new(r"^\d{9}$").unwrap();
+    static ref HCL_RGX: Regex = Regex::new(r"^#[\da-f]{6}$").unwrap();
+}
+
+const HGT_CM_RANGE: RangeInclusive<u16> = RangeInclusive::new(150, 193);
+const HGT_IN_RANGE: RangeInclusive<u16> = RangeInclusive::new(59, 76);
+
 
 fn main() {
     let input_data = load_data().unwrap();
@@ -18,32 +30,24 @@ fn main() {
     part2(&input_data);
 }
 
-fn load_data() -> Result<Vec<HashMap<String, String>>> {
+fn load_data() -> Result<Vec<Passport>> {
     let br = BufReader::new(File::open("./input/input.txt")?);
 
-    let mut passports: Vec<HashMap<String, String>> = Vec::new();
-    let mut passport: HashMap<String, String> = HashMap::new();
+    let mut passports: Vec<Passport> = Vec::new();
+    let mut passport = Passport::new();
+
     for line in br.lines() {
         let l = line.unwrap();
-        let clean_line = l.trim();
+        let clean_line = l.trim().to_string();
 
         if clean_line == "" {
             passports.push(passport);
-            passport = HashMap::new();
+            passport = Passport::new();
             continue;
         }
 
-        for part in clean_line.split(" ") {
-            match part.find(':') {
-                None => panic!("Invalid passport component {:?}", part),
-                Some(index) => {
-                    match (&part[..index], &part[index + 1..]) {
-                        (key, val) => passport.insert(key.to_string(), val.to_string()),
-                        _ => panic!("Unable to split passport component {:?}", part)
-                    };
-                }
-            };
-        }
+        //passport.parse_fields(&clean_line);
+        passport.parse_fields(&clean_line);
     }
 
     // Last passport isn't terminated by a newline ...
@@ -52,69 +56,91 @@ fn load_data() -> Result<Vec<HashMap<String, String>>> {
     Ok(passports)
 }
 
-fn is_passport_well_formed(passport: &HashMap<String, String>) -> bool {
-    let required_fields: Vec<String> = vec_of_strings!["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
-    let mut valid = true;
 
-    for f in required_fields.into_iter() {
-        match passport.get(&f) {
-            None => {valid = false; break;},
-            Some(_) => continue
-        };
-    }
-
-    let key_check: HashSet<String> = passport.keys().cloned().collect(); 
-    if key_check.len() != passport.keys().len() {
-        println!("Duplicate keys detected: is_valid:{} | {:?}", valid, passport);
-    }
-
-    valid
-}
-
-fn part1(input: &Vec<HashMap<String, String>>) {
+fn part1(input: &Vec<Passport>) {
     let mut valid_count = 0;
 
     for passport in input {
-        if is_passport_well_formed(&passport) {
+        if passport.has_required_fields() {
             valid_count += 1;
-        } 
+        }
     }
+
     println!("\nPart 1: Valid Count: {}", valid_count);
 }
 
-fn part2(input: &Vec<HashMap<String, String>>) {
+fn part2(input: &Vec<Passport>) {
     let mut valid_count = 0;
-    let eye_colors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
-    let pid_rgx = Regex::new(r"^\d{9}$").unwrap();
-    let hcl_rgx = Regex::new(r"^#[\da-f]{6}$").unwrap();
-    let hgt_cm_range = RangeInclusive::new(150, 193);
-    let hgt_in_range = RangeInclusive::new(59, 76);
 
     for passport in input {
-        if !is_passport_well_formed(&passport) {
-            continue;
+        if passport.has_required_fields() && passport.has_valid_field_values() {
+            valid_count += 1;
+        }
+    }
+
+    println!("\nPart 2: Valid Count: {}", valid_count);
+}
+
+#[derive(Debug)]
+struct Passport {
+    fields: HashMap<String, String>,
+}
+
+impl Passport {
+    fn new() -> Self {
+        Passport {fields: HashMap::new()}
+    }
+
+    fn parse_fields(&mut self, fields_text: &String) {
+        //let clean_line = fields_text.trim();
+        for part in fields_text.split(" ") {
+            match part.find(':') {
+                None => panic!("Invalid passport component {:?}", part),
+                Some(index) => {
+                    match (&part[..index], &part[index + 1..]) {
+                        (key, val) => self.fields.insert(key.to_string(), val.to_string()),
+                        _ => panic!("Unable to split passport component {:?}", part)
+                    };
+                }
+            };
+        }
+    }
+
+    fn has_required_fields(&self) -> bool {
+        let mut valid = true;
+
+        for f in REQUIRED_FIELDS.iter() {
+            match self.fields.get(f) {
+                None => {valid = false; break;},
+                Some(_) => continue
+            };
         }
 
+        valid
+    }
+
+    fn has_valid_field_values(&self) -> bool {
         let mut valid = true;
-        for (key, val) in passport {
+
+        for (key, val) in self.fields.iter() {
             match key.as_str() {
                 "cid" => continue,
                 "byr" => {
                     // Looks like the *yr values aren't anything but 4 digits so
                     // we can skip the check ...
-                    match u16::from_str(val) {
+                    match u16::from_str(&val) {
                         Ok(v) => if !(1920 <= v && v <= 2002) {valid = false; break;},
                         Err(_) => {valid = false; break;}
                     };
                 },
                 "iyr" => {
-                    match u16::from_str(val) {
+                    match u16::from_str(&val) {
                         Ok(v) => if !(2010 <= v && v <= 2020) {valid = false; break;},
                         Err(_) => {valid = false; break;}
                     };
                 },
                 "eyr" => {
-                    match u16::from_str(val) {
+                    match u16::from_str(&val) {
                         Ok(v) => if !(2020 <= v && v <= 2030) {valid = false; break;},
                         Err(_) => {valid = false; break;}
                     };
@@ -124,8 +150,8 @@ fn part2(input: &Vec<HashMap<String, String>>) {
                     match u16::from_str(h) {
                         Ok(height) => {
                             if unit != "in" && unit != "cm" ||
-                               unit == "cm" && !hgt_cm_range.contains(&height) ||
-                               unit == "in" && !hgt_in_range.contains(&height) {
+                               unit == "cm" && !HGT_CM_RANGE.contains(&height) ||
+                               unit == "in" && !HGT_IN_RANGE.contains(&height) {
                                    valid = false; break;
                             }
                         },
@@ -133,25 +159,21 @@ fn part2(input: &Vec<HashMap<String, String>>) {
                     }
                 },
                 "hcl" => {
-                    if !hcl_rgx.is_match(val) {valid = false; break;}
+                    if !HCL_RGX.is_match(&val) {valid = false; break;}
                 },
                 "ecl" => {
-                    match eye_colors.iter().position(|&v| v == val) {
+                    match EYE_COLORS.iter().position(|v| v == val) {
                         Some(_) => continue,
                         None => {valid = false; break;}
                     }
                 },
                 "pid" => {
-                    if !pid_rgx.is_match(val) {valid = false; break;}
+                    if !PID_RGX.is_match(&val) {valid = false; break;}
                 },
                 _ => panic!("This isn't possible")
             };
         }
 
-        if valid {
-            valid_count += 1;
-        } 
+        valid
     }
-
-    println!("\nPart 2: Valid Count: {}", valid_count);
 }
