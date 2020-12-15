474f9c4fef3f8a9cd98d0c9704a85551ebf2ca1b
Michael Joyce <Michael.J.Joyce@jpl.nasa.gov>
2020-12-15 10:23:56 -0800

    Day5 cleanup and simplification
    Change the boarding pass handling to just convert the string straight to
    a seat id. Didn't catch that the seat id equation was saying "treat this
    10 bit number as a 10 bit number" even though I caught that each line
    was an obfuscated binary value.

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -4,66 +4,31 @@ use std::io::{BufRead, BufReader, Result};
 
 
 fn main() {
-    let input_data = load_data().unwrap();
+    let mut input_data = load_data().unwrap();
+    input_data.sort_unstable();
 
-    part1(&input_data);
-    part2(&input_data);
-}
+    println!("\nPart 1: Max seat id: {}", input_data[input_data.len() - 1]);
 
-fn load_data() -> Result<Vec<String>> {
-    let br = BufReader::new(File::open("./input/input.txt")?);
-    br.lines().collect()
-}
-
-fn part1(input: &[String]) {
-    let mut max_seat_id = 0;
-    
-    for line in input.into_iter() {
-        let row = calculate_row(&line[..7]);
-        let col = calculate_col(&line[7..]);
-        let cur_seat_id = row * 8 + col;
-
-        max_seat_id = max(max_seat_id, cur_seat_id);
-    }
-
-    println!("\nPart 1:\nMax Seat ID {}", max_seat_id);
-}
-
-fn part2(input: &[String]) {
-    println!("\nPart 2:");
-
-    let mut ids: Vec<u32> = Vec::with_capacity(1100);
-    for line in input.into_iter() {
-        let row = calculate_row(&line[..7]);
-        let col = calculate_col(&line[7..]);
-        let cur_seat_id = row * 8 + col;
-        ids.push(cur_seat_id);
-    }
-
-    ids.sort_unstable();
-
-    let mut prev = ids[0] - 1;
-    for id in ids {
-        if id - prev != 1 {
-            println!("Seat id gap found: Prev: {} Cur: {}", prev, id);
-            println!("Seat id: {}", id - 1);
-            break
+    for pair in input_data.windows(2) {
+        if pair[1] - pair[0] == 2 {
+            println!("\nPart 2: Seat id: {}", pair[1] - 1);
         }
-
-        prev = id;
     }
 }
 
-fn calculate_row(row_id: &str) -> u32 {
-    let b_string = row_id.replace("B", "1");
-    let b_string = b_string.replace("F", "0");
+fn load_data() -> Result<Vec<u16>> {
+    let br = BufReader::new(File::open("./input/input.txt")?);
+    let mut lines: Vec<u16> = Vec::new();
 
-    u32::from_str_radix(&b_string, 2).unwrap()
-}
+    for line in br.lines() {
+        let mut val = 0;
+        for (i, c) in line?.chars().rev().enumerate() {
+            if c == 'R' || c == 'B' {
+                val += 2u16.pow(i as u32);
+            }
+        }
+        lines.push(val);
+    }
 
-fn calculate_col(col_id: &str) -> u32 {
-    let b_string = col_id.replace("R", "1");
-    let b_string = b_string.replace("L", "0");
-
-    u32::from_str_radix(&b_string, 2).unwrap()
+    Ok(lines)
 }
