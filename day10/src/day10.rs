051fefd07833ca0a082f9e1abe6b93c90604d375
Michael Joyce <Michael.J.Joyce@jpl.nasa.gov>
2020-12-19 14:07:26 -0800

    Day 10 init

---
+++ day10/src/day10.rs
@@ -0,0 +1,14 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+pub fn load_data() {
+
+}
+
+pub fn part1() {
+
+}
+
+pub fn part2() {
+
+}
---
+++ day10/src/main.rs
@@ -0,0 +1,5 @@
+use day10::day10::{load_data, part1, part2};
+
+fn main() {
+    println!("Hello, world!");
+}
