e887c11004afb0a9455a61c127bc87ca0d7998ea
Michael Joyce <Michael.J.Joyce@jpl.nasa.gov>
2020-12-20 14:37:50 -0800

    Part 1 cleanup
    Update data load and part1 handling to include outlet and device
    joltages and to sort values since part 2 doesn't require and odd
    handling of the input data.

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -3,8 +3,6 @@ use day10::day10::{load_data, part1, part2};
 fn main() {
     let mut input_data = load_data();
 
-    input_data.sort_unstable();
-
     let answer = part1(&input_data);
     println!("\nPart 1: Count 1 differences * Count 3 differences: {}", answer);
 }
