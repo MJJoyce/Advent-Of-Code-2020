65cda0a05b834b1d3290e1d069deab3fc0b4eed4
Michael Joyce <Michael.J.Joyce@jpl.nasa.gov>
2020-12-20 14:33:37 -0800

    Initial day10 part 1

--- day10/src/day10.rs
+++ day10/src/day10.rs
@@ -1,14 +1,51 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::str::FromStr;
 
-pub fn load_data() {
 
+pub fn load_data() -> Vec<u64> {
+    let br = BufReader::new(File::open("./input/input.txt").unwrap());
+    br.lines().map(|l| l.unwrap().trim().parse::<u64>().unwrap()).collect()
 }
 
-pub fn part1() {
+pub fn part1(input: &Vec<u64>) -> u64 {
+    let mut d_1 = 0;
+    let mut d_3 = 0;
 
+    // Don't forget joltage difference between outlet (0) and first adapter!
+    if input[0] == 1 {
+        d_1 += 1;
+    } else if input[0] == 3 {
+        d_3 += 1;
+    }
+    
+
+    for window in input.windows(2) {
+        println!("Next pair: {:?}. D1: {} | D3: {}", window, d_1, d_3);
+
+        match window[1] - window[0] {
+            1 => {d_1 += 1; println!("Delta 1");},
+            3 => {d_3 += 1; println!("Delta 3");},
+            _ => ()
+        }
+    }
+
+    // Don't forget the device's built in joltage adapter which is always
+    // 3 higher than the highest adapter!
+    d_1 * (d_3 + 1)
 }
 
 pub fn part2() {
+}
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_p1() {
+        let mut input = vec![16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4];
+        input.sort();
+        assert_eq!(part1(&input), 35);
+    }
 }
--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,5 +1,10 @@
 use day10::day10::{load_data, part1, part2};
 
 fn main() {
-    println!("Hello, world!");
+    let mut input_data = load_data();
+
+    input_data.sort_unstable();
+
+    let answer = part1(&input_data);
+    println!("\nPart 1: Count 1 differences * Count 3 differences: {}", answer);
 }
