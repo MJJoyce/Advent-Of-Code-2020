fb716ae3701a22445459b3fac004e15d30b24a5f
Michael Joyce <Michael.J.Joyce@jpl.nasa.gov>
2020-12-23 15:47:52 -0800

    Init day 12

---
+++ day12/src/day12.rs
@@ -0,0 +1,11 @@
+pub fn load_data() {
+
+}
+
+pub fn part1() {
+
+}
+
+pub fn part2() {
+
+}
---
+++ day12/src/main.rs
@@ -0,0 +1,5 @@
+use day12::day12::{load_data, part1, part2};
+
+fn main() {
+    println!("Hello, world!");
+}
